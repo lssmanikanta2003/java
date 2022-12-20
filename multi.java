66664dcb0cdd1545876f0c35a1da1066ea4eb670
lssmanikanta2003 <121091323+lssmanikanta2003@users.noreply.github.com>
2022-12-20 14:42:30 +0530

    Create multi.java

---
+++ multi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class multi
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter 2 numbers:");
+int a=s.nextInt();
+int b=s.nextInt();
+System.out.println("the result is:"+(a*b));
+}
+}
